6c760225277a03937d1a7dc859888ff7694ec8c6
olivier.parraud <olivier.parraud@laplateforme.io>
2026-01-16 11:01:44 +0100

    ajout button

---
+++ mon-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from './components/Button/Button'
+
+function App() {
+  return (
+    <div className="App">
+      <Button text='Je suis un bouton' />
+    </div>
+  );
+}
+
+export default App;
