76daf23143810dc05c8bd459eaa741d8e62b1b49
olivier.parraud <olivier.parraud@laplateforme.io>
2026-01-19 16:46:48 +0100

    ajout flipped
    ajout dos de carte, shuffled

--- mon-app/src/components/Card/Card.js
+++ mon-app/src/components/Card/Card.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./card.css";
 
 function Card(props) {
@@ -16,33 +16,54 @@ function Card(props) {
         '/assets/img/9.jpg',
 
     ];
+
+    const dos = [
+        
+        '/assets/img/dos.jpg',
+    ]
+
+    // Fonction pour mélanger un tableau
+    const shuffleArray = (array) => {
+        const shuffled = [...array];
+        for (let i = shuffled.length -1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            // Math regroupe toutes les fonctions de maths -- floor lui, arondi au plus grand entier.
+            [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+        }
+        return shuffled;
+    };
+
+    // Créer les paires et mélanger
+    const [shuffledImages, setShuffledImages] = useState([]);
+    const [flippedCards, setFlippedCards] = useState([]);
+
+    useEffect(() => {
+        const pairs = images.flatMap(img => [img, img]);
+        // Map parcourt un tableau et ça récréer un nouveau tableau.
+        setShuffledImages(shuffleArray(pairs));
+    },  []);
+
+    const handleCardClick = (index) => {
+        if (!flippedCards.includes(index)) {
+            setFlippedCards([...flippedCards, index])
+        }
+    };
+
     return (
         <div className='card'>
             {
-                images.map((name) =>
-                    <>
-                        <img className="card-img" src={name}></img>
-                        <img className="card-img" src={name}></img>
-                    </>
-
-
-
+                shuffledImages.map((name,index) =>
+                    <img 
+                    key={index} 
+                    className="card-img" 
+                    src={flippedCards.includes(index) ? name : dos} 
+                    alt=""
+                    onClick={() => handleCardClick(index)}/>
                 )
-
             };
-
-
         </div>
     )
-
-
-
 }
-
-
-
-
-
 
 export default Card;
 
