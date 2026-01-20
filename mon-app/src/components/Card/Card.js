62614bec1f21f267fc8ed9c8d1a70f5556e6ebfa
olivier.parraud <olivier.parraud@laplateforme.io>
2026-01-20 13:07:16 +0100

    maj

--- mon-app/src/components/Card/Card.js
+++ mon-app/src/components/Card/Card.js
@@ -17,15 +17,13 @@ function Card(props) {
 
     ];
 
-    const dos = [
-        
-        '/assets/img/dos.jpg',
-    ]
+    const dos = '/assets/img/dos.jpg';
+
 
     // Fonction pour mélanger un tableau
     const shuffleArray = (array) => {
         const shuffled = [...array];
-        for (let i = shuffled.length -1; i > 0; i--) {
+        for (let i = shuffled.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
             // Math regroupe toutes les fonctions de maths -- floor lui, arondi au plus grand entier.
             [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
@@ -36,29 +34,58 @@ function Card(props) {
     // Créer les paires et mélanger
     const [shuffledImages, setShuffledImages] = useState([]);
     const [flippedCards, setFlippedCards] = useState([]);
+    const [matchedCards, setMatchedCards] = useState([]);
+    const [isChecking, setIsChecking] = useState(false);
 
     useEffect(() => {
         const pairs = images.flatMap(img => [img, img]);
         // Map parcourt un tableau et ça récréer un nouveau tableau.
         setShuffledImages(shuffleArray(pairs));
-    },  []);
+    }, []);
+
+    useEffect(() => {
+        if (flippedCards.length === 2) {
+            setIsChecking(true);
+            const [firstIndex, secondIndex] = flippedCards;
+
+            if (shuffledImages[firstIndex] === shuffledImages[secondIndex]) {
+                // Les cartes correspondent
+                setMatchedCards([...matchedCards, firstIndex, secondIndex]);
+                setFlippedCards([]);
+                setIsChecking(false);
+            } else {
+                // les cartes ne correspondent pas
+                setTimeout(() => {
+                    setFlippedCards([]);
+                    setIsChecking(false);
+                }, 1000);
+            }
+        }
+    }, [flippedCards]);
+
 
     const handleCardClick = (index) => {
-        if (!flippedCards.includes(index)) {
-            setFlippedCards([...flippedCards, index])
+        if (
+            isChecking === false &&
+            flippedCards.length < 2 &&
+            !flippedCards.includes(index) &&
+            !matchedCards.includes(index)
+        ) {
+            setFlippedCards([...flippedCards, index]);
         }
     };
+
 
     return (
         <div className='card'>
             {
-                shuffledImages.map((name,index) =>
-                    <img 
-                    key={index} 
-                    className="card-img" 
-                    src={flippedCards.includes(index) ? name : dos} 
-                    alt=""
-                    onClick={() => handleCardClick(index)}/>
+                shuffledImages.map((name, index) =>
+                    <img
+                        key={index}
+                        className="card-img"
+                        src={flippedCards.includes(index) || matchedCards.includes(index) ? name : dos}
+                        alt=""
+                        onClick={() => handleCardClick(index)} />
                 )
             };
         </div>
